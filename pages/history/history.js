6d8661a7ec1a119c88ffdaa6fb1f8987a1f7db20
zeze.li <lizeze.boy@gmail.com>
2021-02-28 20:54:01 +0800

    增加结余

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,54 +1,38 @@
 // pages/history/history.js
+const util = require('../../utils/util.js')
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
+    currentMonth: 0,
+    currentYear: 0
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
+
   onLoad: function (options) {
+
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth() + 1
+    this.setData({
+      currentMonth: month,
+      currentYear: year
+
+
+    })
+    let result = await util.httpRequest({
+      url: 'api/bill/' + '/' + year + "/" + month
+    })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  },
+ getData:function(year,month){
+
+  
+ },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  },
+  onShow: function () {}
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  }
 })
